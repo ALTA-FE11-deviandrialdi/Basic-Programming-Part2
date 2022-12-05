a0cc550a13343844c162e43b357d6d318f1d5d3d
Devi Andri Aldi <73923081+deviandrialdi@users.noreply.github.com>
2022-12-05 18:56:41 +0700

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -1,5 +1,12 @@
 function primeNumber(number) {
   // Your Code Here
+  
+   let pembagi = 0
+    for(let i = 1; i <= number; i++){
+        number % i === 0 && pembagi++
+    }
+
+    return pembagi === 2
 }
 
 console.log(primeNumber(11)); // true
